090d0925789651a2ba83669ce5cd9a507266a480
Albert Herrero Barros <albertherrero29@gmail.com>
2026-04-20 18:30:41 +0200

    Estructura inicial - Apps

---
+++ apps/matchdays/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MatchdaysConfig(AppConfig):
+    name = 'apps.matchdays'
--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'users',
+    'leagues',
+    'players',
+    'market',
+    'matchdays',
+    'ranking',
 ]
 
 MIDDLEWARE = [
